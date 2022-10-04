22ccb053336096994bbbd24d780654400df1a1da
KatyKedi <katykat5bros@gmail.com>
2022-10-04 08:21:27 -0700

    seed files created

--- seeds/index.js
+++ seeds/index.js
@@ -1,4 +1,7 @@
 const seedUsers = require('./user-seeds');
+const seedEffects = require('./effect-seeds.js');
+const seedPotions = require('./potion-seeds');
+const seedIngredients = require('./ingredient-seeds')
 const sequelize = require('../config/connection');
 
 const seedAll = async () => {
@@ -6,7 +9,12 @@ const seedAll = async () => {
   console.log('\n----- DATABASE SYNCED -----\n');
   await seedUsers();
   console.log('\n----- USERS SEEDED -----\n');
-
+  await seedEffects();
+  console.log('\n----- EFFECTS SEEDED -----\n');
+  await seedPotions();
+  console.log('\n----- POTIONS SEEDED -----\n');
+  await seedIngredients();
+  console.log('\n----- INGREDIENTS SEEDED -----\n');
   process.exit(0);
 };
 
